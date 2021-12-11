10cb67d737e8447f428ccceeea9cde4276b5f74b
Monte-M <mantasmate@gmail.com>
2021-12-11 11:41:41 +0200

    code changes on items api

--- src/routes/items.js
+++ src/routes/items.js
@@ -4,12 +4,16 @@ const { dbAction, dbFail, dbSuccess } = require('../utils/dbHelper');
 const { authenticateToken } = require('../utils/middleware');
 const router = express.Router();
 
-// GET /item/:id
-router.get('/:id', async (req, res) => {
+// GET all user ads
+router.get('/user-items/:id', authenticateToken, async (req, res) => {
   const { id } = req.params;
   if (!id) return dbFail(res, 'bad input', 400);
   const sql = `
-  SELECT * FROM items WHERE id = ?
+  SELECT items.id, items.category_id, items.title, items.description, items.city, items.price, items.item_condition, items.image, items.post_timestamp, categories.category
+  FROM items
+  INNER JOIN categories
+  ON items.category_id = categories.id
+  WHERE user_id = ?
     `;
   const dbResult = await dbAction(sql, [id]);
   if (dbResult === false) return dbFail(res);
@@ -18,11 +22,34 @@ router.get('/:id', async (req, res) => {
 
 // // GET all items
 router.get('/', async (req, res) => {
-  const dbResult = await dbAction('SELECT * FROM items');
+  const dbResult = await dbAction(`
+  SELECT items.id, items.category_id, items.title, items.description, items.city, items.price, items.item_condition, items.image, items.post_timestamp, categories.category
+  FROM items
+  INNER JOIN categories
+  ON items.category_id = categories.id
+`);
   if (dbResult === false) {
     return dbFail(res, 'error getting items');
   }
   return dbSuccess(res, dbResult);
+});
+
+// GET /item/:id
+router.get('/:id', async (req, res) => {
+  const { id } = req.params;
+  if (!id) return dbFail(res, 'bad input', 400);
+  const sql = `
+  SELECT items.id, items.title, items.description, items.city, items.price, items.item_condition, items.image, items.post_timestamp, users.id AS 'user_id', users.username, users.email, users.city AS 'user_city', users.phone_number, users.image AS 'user_image', users.reg_timestamp, categories.category
+  FROM items
+  INNER JOIN users
+  ON items.user_id = users.id
+  INNER JOIN categories
+  ON items.category_id = categories.id
+  WHERE items.id = ?
+    `;
+  const dbResult = await dbAction(sql, [id]);
+  if (dbResult === false) return dbFail(res);
+  dbSuccess(res, dbResult);
 });
 
 // POST new item
@@ -52,6 +79,17 @@ router.post('/', authenticateToken, async (req, res) => {
     return res.status(500).json({ error: 'something went wrong' });
   }
   res.json({ msg: 'item added' });
+});
+
+router.delete('/delete/:id', authenticateToken, async (req, res) => {
+  const { id } = req.params;
+  if (!id) return dbFail(res, 'bad input', 400);
+  const sql = `
+  DELETE FROM items WHERE items.id = ?
+    `;
+  const dbResult = await dbAction(sql, [id]);
+  if (dbResult === false) return dbFail(res);
+  dbSuccess(res, dbResult);
 });
 
 module.exports = router;
